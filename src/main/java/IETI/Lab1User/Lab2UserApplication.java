323610f897ad4b19ac04b85a9b9785e8d8a85f6e
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-09-04 10:42:37 -0500

    creacion avances del proyecto

---
+++ src/main/java/IETI/Lab1User/Lab2UserApplication.java
@@ -0,0 +1,13 @@
+package IETI.Lab1User;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2UserApplication.class, args);
+	}
+
+}
---
+++ src/main/java/IETI/Lab1User/service/UserService.java
@@ -0,0 +1,16 @@
+package IETI.Lab1User.service;
+import IETI.Lab1User.entities.User;
+import java.util.List;
+
+public interface UserService
+{
+    User create( User user );
+
+    User findById( String id );
+
+    List<User> getAll();
+
+    void deleteById( String id );
+
+    User update(User user, String userId );
+}
